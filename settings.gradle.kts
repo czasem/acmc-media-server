3af8c7d4447fd35463751d5416f844e832f2f5db
czasem <102756369+czasem@users.noreply.github.com>
2025-02-05 21:24:17 +0100

    cipka

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "acmc-media-server"
+
---
+++ src/main/java/pl/czasem/template/Main.java
@@ -0,0 +1,7 @@
+package pl.czasem.template;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
